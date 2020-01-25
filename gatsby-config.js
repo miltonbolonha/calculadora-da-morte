2242baf91bff520a41513acebdde65629e95f09d
bolonhadev <fala@bolonha.dev>
2020-01-25 02:05:32 -0300

    CM-04: Bugfix/theme_color

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
         short_name: `starter`,
         start_url: `/`,
         background_color: `#000000`,
-        theme_color: `#d61d1d`,
+        theme_color: `#1f3253`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
